1c179868d8c03819a9df28e0e8817ee331ac5d77
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-09 21:33:05 +0900

    Create Project

---
+++ JustDoIt/JustDoIt/JustDoItApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustDoItApp.swift
+//  JustDoIt
+//
+//  Created by 양정연 on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct JustDoItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
