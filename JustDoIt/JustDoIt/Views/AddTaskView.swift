04b2e2f49cb8d4479b575e8775976cbb4f0e6405
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-10 21:24:30 +0900

    Style 할 일 추가뷰 safe area 배경색 적용

--- JustDoIt/JustDoIt/Views/AddTaskView.swift
+++ JustDoIt/JustDoIt/Views/AddTaskView.swift
@@ -13,41 +13,44 @@ struct AddTaskView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Text("할 일 추가하기")
-                .foregroundColor(.white)
-                .font(.title).bold()
-                .frame(maxWidth: .infinity, alignment: .leading)
-            HStack {
-                TextField("", text: $taskTitle, prompt: Text("여기에 해야 할 일을 적어주세요").foregroundColor(.gray))
-                    .foregroundColor(.white)
+        Color(uiColor: .background)
+            .ignoresSafeArea()
+            .overlay(
+                VStack(alignment: .center, spacing: 20) {
+                    Text("할 일 추가하기")
+                        .foregroundColor(.white)
+                        .font(.title).bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    HStack {
+                        TextField("", text: $taskTitle, prompt: Text("여기에 해야 할 일을 적어주세요").foregroundColor(.gray))
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
                     .background(Color.black)
-            }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 10)
-            .background(Color.black)
-            .cornerRadius(10)
-            
-            Spacer()
-            
-            Button {
-                if !taskTitle.isEmpty {
-                    realmManager.addTask(taskTitle: taskTitle)
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                    
+                    Button {
+                        if !taskTitle.isEmpty {
+                            realmManager.addTask(taskTitle: taskTitle)
+                        }
+                        dismiss()
+                    } label: {
+                        Text("추가하기")
+                            .foregroundColor(.white)
+                            .font(.headline).bold()
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.accentColor)
+                            .cornerRadius(50)
+                    }
                 }
-                dismiss()
-            } label: {
-                Text("추가하기")
-                    .foregroundColor(.white)
-                    .font(.headline).bold()
-                    .padding()
+                    .padding(.top, 40)
                     .padding(.horizontal)
-                    .background(Color.accentColor)
-                    .cornerRadius(50)
-            }
-        }
-        .padding(.top, 40)
-        .padding(.horizontal)
-        .background(Color(uiColor: .background))
+            )
     }
 }
 
