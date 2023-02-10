275ee85c46f3b468fc9fa57485f53981646a294a
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-10 15:50:05 +0900

    Feat 할 일 추가 뷰 UI

---
+++ JustDoIt/JustDoIt/Views/AddTaskView.swift
@@ -0,0 +1,51 @@
+//
+//  AddTaskView.swift
+//  JustDoIt
+//
+//  Created by 양정연 on 2023/02/10.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    @State private var taskName: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("할 일 추가하기")
+                .foregroundColor(.white)
+                .font(.title).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            HStack {
+                TextField("", text: $taskName, prompt: Text("여기에 해야 할 일을 적어주세요").foregroundColor(.gray))
+                    .foregroundColor(.white)
+                    .background(Color.black)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Color.black)
+            .cornerRadius(10)
+            
+            Button {
+                print("Create Button Did Tap!")
+            } label: {
+                Text("추가하기")
+                    .foregroundColor(.white)
+                    .font(.headline).bold()
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.accentColor)
+                    .cornerRadius(50)
+            }
+        }
+        .padding(.top, 40)
+        .padding(.horizontal)
+        .background(Color(uiColor: .background))
+    }
+}
+
+struct AddTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTaskView()
+    }
+}
