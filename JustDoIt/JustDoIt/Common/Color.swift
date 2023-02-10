8c4b6d223f57c78d37daf92be75d09daa66a2c10
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-10 15:24:10 +0900

    Add 앱 아이콘 및 컬러 에셋

---
+++ JustDoIt/JustDoIt/Common/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  JustDoIt
+//
+//  Created by 양정연 on 2023/02/10.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    class var accent: UIColor! { return UIColor(named: "AccentColor") }
+    class var background: UIColor! { return UIColor(named: "Background") }
+}
