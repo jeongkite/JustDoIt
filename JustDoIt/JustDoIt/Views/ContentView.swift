7f5645f160ff70b6bba8ea780c9780afbfca0b36
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-10 16:15:39 +0900

    Feat 할 일 추가 화면전환

--- JustDoIt/JustDoIt/Views/ContentView.swift
+++ JustDoIt/JustDoIt/Views/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowAddTaskView: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .bottomTrailing) {
+            TasksView()
+            
+            MainAddButton()
+                .padding()
+                .onTapGesture {
+                    isShowAddTaskView.toggle()
+                }
         }
-        .padding()
+        .sheet(isPresented: $isShowAddTaskView) {
+            AddTaskView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .background(Color(uiColor: .background))
     }
 }
 
