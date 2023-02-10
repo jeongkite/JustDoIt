91596e0e0c1f4ecb3e7ee159d37c0c48068e902f
Jeongyeon Yang <jeong6kite@gmail.com>
2023-02-10 16:08:19 +0900

    Feat 할 일 추가 버튼 UI

---
+++ JustDoIt/JustDoIt/Components/MainAddButton.swift
@@ -0,0 +1,30 @@
+//
+//  MainAddButton.swift
+//  JustDoIt
+//
+//  Created by 양정연 on 2023/02/10.
+//
+
+import SwiftUI
+
+struct MainAddButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 50)
+                .foregroundColor(Color.accentColor)
+            Image(systemName: "pencil")
+                .resizable()
+                .foregroundColor(.white)
+                .scaledToFit()
+                .frame(width: 24)
+        }
+        .frame(height: 50)
+    }
+}
+
+struct MainAddButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MainAddButton()
+    }
+}
